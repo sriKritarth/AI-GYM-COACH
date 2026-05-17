0f07e231c3a004d3741771e6f08069ab43ff0d0b
sriKritarth <oftheleaf.hidden@gmail.com>
2026-05-18 00:41:48 +0530

    Enabled text-to-speech fuunctionality

---
+++ services/coaching/llm.py
@@ -0,0 +1,32 @@
+from services.config.workout_config import PROMPT
+
+
+class LLMCoach:
+    def __init__(self , groq_client):
+        self.client = groq_client,
+        self.history=  []
+        self.system_prompt = PROMPT
+
+    def give_feedback(self , event , issues):
+        prompt = f"Event : {event}"
+
+        if issues:
+            prompt += f"Form issue {issues}"
+
+
+        message = [
+            {"role" : "system" , "content" : self.system_prompt},
+            *self.history[-10:],
+            {"role" : "user" , "content" : prompt}
+        ]
+
+        response = self.client.chat.completions.create(
+            model = "llama-3.3-70b-versatile",
+            messages = message,
+            temperature = 0.4
+        )
+
+        text = response.choices[0].message.content.strip()
+        self.history.append({"role": "assistant", "content": text})
+
+        return text
